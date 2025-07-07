228c4e735ab72249019e6a11ec56890417fd62d6
Cam-God <camerongodwin252@gmail.com>
2025-07-07 17:32:03 +0300

    feat: pagination for anomalies panel

--- frontend/src/components/AnomaliesPanel.jsx
+++ frontend/src/components/AnomaliesPanel.jsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 
 function AnomaliesPanel() {
   const [anomalies, setAnomalies] = useState([]);
+  const [displayedLogs, setDisplayedLogs] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const LOGS_PER_PAGE = 10;
+  const totalPages = Math.ceil(anomalies.length / LOGS_PER_PAGE);
 
   useEffect(() => {
     fetch("/anomalies")
@@ -24,6 +28,21 @@ function AnomaliesPanel() {
 
     return () => socket.close();
   }, []);
+
+  // Update displayedLogs when logs or page changes
+  useEffect(() => {
+    const start = (currentPage - 1) * LOGS_PER_PAGE;
+    const end = start + LOGS_PER_PAGE;
+    setDisplayedLogs(anomalies.slice(start, end));
+  }, [anomalies, currentPage]);
+
+  const handlePage = (direction) => {
+    if (direction === "prev" && currentPage > 1) {
+      setCurrentPage((prev) => prev - 1);
+    } else if (direction === "next" && currentPage < totalPages) {
+      setCurrentPage((prev) => prev + 1);
+    }
+  };
 
   return (
     <div className="p-2 mt-6 border border-red-500 rounded">
@@ -53,32 +72,61 @@ function AnomaliesPanel() {
               </tr>
             </thead>
             <tbody>
-              {anomalies.map((log, index) => (
-                <tr key={index}>
-                  <td className="border px-1 border-red-300 small-text">
-                    {log.timestamp}
-                  </td>
-                  <td className="border px-1 border-red-300 small-text">
-                    {log.service}
-                  </td>
-                  <td className="border px-1 border-red-300 small-text">
-                    {log.level}
-                  </td>
-                  <td className="border px-1 font-semibold text-red-500 border-red-300 small-text">
-                    {log.flag || "No flag in scope"}
-                  </td>
-                  <td className="border px-1 font-semibold text-red-500 border-red-300 small-text">
-                    {log.message}
-                  </td>
-                </tr>
-              ))}
+              {displayedLogs &&
+                displayedLogs.map((log, index) => (
+                  <tr key={index}>
+                    <td className="border px-1 border-red-300 small-text">
+                      {log.timestamp}
+                    </td>
+                    <td className="border px-1 border-red-300 small-text">
+                      {log.service}
+                    </td>
+                    <td className="border px-1 border-red-300 small-text">
+                      {log.level}
+                    </td>
+                    <td className="border px-1 font-semibold text-red-500 border-red-300 small-text">
+                      {log.flag || "No flag in scope"}
+                    </td>
+                    <td className="border px-1 font-semibold text-red-500 border-red-300 small-text">
+                      {log.message}
+                    </td>
+                  </tr>
+                ))}
             </tbody>
           </table>
-          <div className="page-section">
-            <div className="page-arrow">&lt;</div>
-            <div className="page-number">250</div>
-            <div className="page-arrow">&gt;</div>
-          </div>
+          {totalPages > 1 && (
+            <div className="page-section">
+              <div onClick={() => handlePage("prev")} className="page-arrow">
+                &lt;
+              </div>
+              {currentPage > 1 && (
+                <div
+                  onClick={() => setCurrentPage(currentPage - 1)}
+                  className="page-number"
+                >
+                  {currentPage - 1}
+                </div>
+              )}
+              <div className="page-number current-page-number">
+                {currentPage}
+              </div>
+              {totalPages > currentPage && (
+                <div
+                  onClick={() => setCurrentPage(currentPage + 1)}
+                  className="page-number"
+                >
+                  {currentPage + 1}
+                </div>
+              )}
+              <div
+                onClick={() => handlePage("next")}
+                disabled={currentPage === totalPages}
+                className="page-arrow"
+              >
+                &gt;
+              </div>
+            </div>
+          )}
         </div>
       )}
     </div>
